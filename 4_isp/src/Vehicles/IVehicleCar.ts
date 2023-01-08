059b7c287e16027ff6940ae75b3ed48281411ed9
max.aguiar <max.aguiar@ciss.com.br>
2023-01-08 02:55:07 -0300

    Interface Segregation Principle - ISP

---
+++ 4_isp/src/Vehicles/IVehicleCar.ts
@@ -0,0 +1,11 @@
+import IVehicle from "./IVehicle";
+
+export default interface IVehicleCar extends IVehicle {
+  configurationCar(
+    color: string,
+    year: number,
+    engine: number,
+    seats: number,
+    doors: number
+  ): void;
+}
